387040815e792608d5ad4e2126467a7902cde127
Ottobong <clemency50@gmail.com>
2023-04-30 02:04:03 +0100

    updated the registration page and formInput

--- src/component/formInput/FormInput.jsx
+++ src/component/formInput/FormInput.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './formInput.scss';
 
 const FormInput = (props) => {
+  const [focused, setFocused] = useState(false);
+  const { errorMessage, onChange, id, ...inputProps } = props;
+
+  const handleFocus= (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div className='formInput'>
-      <input placeholder={props.placeholder}/>
+<div className = "formInput">
+      <input 
+        {...inputProps} 
+        onChange={onChange} 
+        onFocus = {()=>inputProps.name === "confirmPassword" && setFocused(true)}
+        onBlur={handleFocus} 
+        focused = {focused.toString()}/>
+      <span className='errorMessage'>{errorMessage}</span>
     </div>
   )
 }
